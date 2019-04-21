5b682110331bacdaed959b2e7228448cb1c1d237
kanzul12 <sp19bsse0029@MAJU.EDU.PK>
2019-04-21 14:23:41 +0500

    it is my first task

---
+++ loop.py
@@ -0,0 +1,24 @@
+num= int (input("enter number of rows="))
+for  i in range (1,num+1):
+    for j in range(1,num-i+1):
+            print (" ",end="")
+    for j in range(2 and 9):
+        print("2","9")
+
+for i in range(1, 6):
+        for j in range(1, 10):
+                if i==5 or i+j==5 or j-i==4:
+                        print("*", end="")
+                else:
+                        print(end=" ")
+        print()
+
+
+
+
+
+
+
+
+
+
