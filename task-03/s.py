1c6ff108b1008c9d708d9609246765ac216b9c5d
saanaa96 <49777506+saanaa96@users.noreply.github.com>
2019-05-05 06:43:56 -0700

    Add files via upload

---
+++ task-03/s.py
@@ -0,0 +1,18 @@
+
+    
+
+sum=0.0
+total=0.0
+print("enter number or")
+print("enter x")
+num =int(input("enter number from 1 to  59 or variable x"))
+while(num != x ):
+    if(num>0):
+
+        sum =sum+num
+    else:
+        total =total+num
+    
+
+print("the total of postive is",sum)
+print("the total of negative is",total)
