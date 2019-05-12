636bedf788d5f992c83b66545fbd97ec699be4eb
kanzul12 <49777342+kanzul12@users.noreply.github.com>
2019-05-12 19:44:23 +0500

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+import webbrowser as wb 
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source: 
+    print('say something')
+    audio =r.listen(source)
+    print('done')
+   
+   
+try:
+    text = r.recognize_google(audio)
+    print('google think you said:\n'+text)
+            
+except Exception as e: 
+    print(e)
+
